f5b53be0f85003d3bbe522bd1607dbfd6fd3b32c
shervin17 <135480875+shervin17@users.noreply.github.com>
2024-05-24 18:48:49 +0800

    added payrollForm

---
+++ PayrollForm.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PayrollV1
+{
+    partial class PayrollForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.income_field = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tax_field = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // income_field
+            // 
+            this.income_field.Location = new System.Drawing.Point(116, 18);
+            this.income_field.Name = "income_field";
+            this.income_field.Size = new System.Drawing.Size(149, 22);
+            this.income_field.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "taxable income";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(440, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "generate tax";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "witholding tax";
+            // 
+            // tax_field
+            // 
+            this.tax_field.Location = new System.Drawing.Point(116, 53);
+            this.tax_field.Name = "tax_field";
+            this.tax_field.Size = new System.Drawing.Size(149, 22);
+            this.tax_field.TabIndex = 4;
+            // 
+            // PayrollForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tax_field);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.income_field);
+            this.Name = "PayrollForm";
+            this.Text = "PayrollForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox income_field;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tax_field;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,9 +17,11 @@ namespace PayrollV1
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PayrollForm());
+
             /* Application.Run(new ProductForm());*/
             /* Application.Run(new Login());*/
-            Application.Run(new Employee_login_form());
+           /* Application.Run(new Employee_login_form());*/
             /* Application.Run(new DTRForm());*/
         }
     }
