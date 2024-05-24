aa9abe7f15248196895f1ee05d23ef92e450ea5f
shervin17 <135480875+shervin17@users.noreply.github.com>
2024-05-25 03:01:02 +0800

    added SSS,PagIbig and Philhealth

---
+++ SSSContributionCalculator.cs
@@ -0,0 +1,35 @@
+﻿using PayrollV1.Models.Employee;
+using System;
+
+namespace PayrollV1
+{
+    public class SSSContributionCalculator
+    {
+        private const decimal MinMonthlySalaryCredit = 4000m;
+        private const decimal MaxMonthlySalaryCredit = 30000m;
+        private const decimal EmployerShareRate = 0.095m;
+        private const decimal EmployeeShareRate = 0.045m;
+        private const decimal TotalContributionRate = 0.14m;
+
+        public static SSSContribution ComputeSSSContribution(decimal income)
+        {
+            decimal MSC = income < MinMonthlySalaryCredit ? MinMonthlySalaryCredit : (income > MaxMonthlySalaryCredit ? MaxMonthlySalaryCredit : income);
+
+            SSSContribution sssContribution = new SSSContribution
+            {
+                EmployerShare = Math.Round(MSC * EmployerShareRate, 2),
+                EmployeeShare = Math.Round(MSC * EmployeeShareRate, 2),
+                TotalContribution = Math.Round(MSC * TotalContributionRate, 2)
+            };
+
+            return sssContribution;
+        }
+    }
+
+    public class SSSContribution
+    {
+        public decimal EmployeeShare { get; set; }
+        public decimal EmployerShare { get; set; }
+        public decimal TotalContribution { get; set; }
+    }
+}
